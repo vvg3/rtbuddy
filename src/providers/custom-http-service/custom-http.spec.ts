8f3ad919813e71ce56fe42b76e4641fa25e3d965
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-03 19:32:08 -0400

    added tests

--- src/providers/custom-http-service/custom-http.spec.ts
+++ src/providers/custom-http-service/custom-http.spec.ts
@@ -37,4 +37,46 @@ describe('CustomHttpService', () => {
         });
         httpMock.verify();
     });
+
+    it('should post the correct data', () => {
+        service.post<any>({ firstname: 'firstname' }).subscribe((data:any) => {
+            expect(data.firstname).toBe('firstname');
+        });
+
+        const req = httpMock.expectOne(`www.espn.go.com`, 'post to api');
+        expect(req.request.method).toBe('POST');
+        req.flush({
+            firstname: 'firstname'
+        });
+
+        httpMock.verify();
+    });
+
+    it('should put the correct data', () => {
+        service.put<any>(3, { firstname: 'firstname' }).subscribe((data: any) => {
+            expect(data.firstname).toBe('firstname');
+        });
+
+        const req = httpMock.expectOne(`www.espn.go.com/3`, 'put to api');
+        expect(req.request.method).toBe('PUT');
+
+        req.flush({
+            firstname: 'firstname'
+        });
+        httpMock.verify();
+    });
+
+    it('should delete the correct data', () => {
+        service.delete(3).subscribe((data: any) => {
+            expect(data).toBe(3);
+        });
+
+        const req = httpMock.expectOne(`www.espn.go.com/3`, 'delete to api');
+        expect(req.request.method).toBe('DELETE');
+
+        req.flush(3);
+
+        httpMock.verify();
+    });
+
 });
